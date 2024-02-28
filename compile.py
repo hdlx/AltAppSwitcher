02bf4a480761efe99cf27063412ff52c1cdd637b
Paul HUBERT <hamtarodeluxe@gmail.com>
2024-02-28 14:25:16 +0100

    Added -s argument to relese commandline

--- compile.py
+++ compile.py
@@ -17,6 +17,7 @@ if not os.path.exists("./Output/Debug"):
 link = "-l dwmapi -l User32 -l Gdi32 -l Gdiplus"
 gccCmdDbg = "gcc {0} -o Output/Debug/MacAppSwitcher.exe -Werror -ggdb -I ./Sources {1} -march=x86-64".format(cFiles, link)
 gccCmdRel = "gcc {0} -o Output/Release/MacAppSwitcher.exe -mwindows -I ./Sources {1} -march=x86-64".format(cFiles, link)
+gccCmdRel = "gcc {0} -o Output/Release/MacAppSwitcher.exe -mwindows -s -I ./Sources {1} -march=x86-64".format(cFiles, link)
 
 os.system(gccCmdDbg)
 os.system(gccCmdRel)
