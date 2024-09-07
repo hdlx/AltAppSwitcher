2285f193b624f67de58c76b744651ee6666347bf
hdlx <hamtarodeluxe@gmail.com>
2024-09-07 14:26:12 +0200

    Minor script improvements

--- Assets/RemoveFromStartup.bat
+++ Assets/RemoveFromStartup.bat
@@ -3,9 +3,9 @@ REM https://superuser.com/questions/788924/is-it-possible-to-automatically-run-a
 
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
-if '%errorlevel%' NEQ '0' (
+if not ERRORLEVEL == 0 (
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c """"%~s0"" """, "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" ", "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
@@ -14,12 +14,12 @@ if '%errorlevel%' NEQ '0' (
 cd /d "%~dp0"
 
 schtasks /delete /tn AltAppSwitcher /f
-if ERRORLEVEL == 1 (
+if not ERRORLEVEL == 0 (
     msg * "No startup task to remove."
     exit
 )
 reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "AltAppSwitcher" /f
-if ERRORLEVEL == 1 (
+if not ERRORLEVEL == 0 (
     msg * "No startup task to remove."
     exit
 )
