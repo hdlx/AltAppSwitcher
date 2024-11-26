554c8db4083a3cd99941f3f4585c7d017a924c52
hdlx <hamtarodeluxe@gmail.com>
2024-11-26 21:21:24 +0100

    Created settings project. Removed useless py script

---
+++ Sources/Settings/Settings.c
@@ -0,0 +1,6 @@
+#include <minwindef.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+    (void)hInstance; (void)hPrevInstance; (void)nCmdShow; (void)pCmdLine;
+    return 0;
+}
--- compile.py
+++
@@ -1,5 +0,0 @@
-import compileCommon
-compileCommon.CompileDbg("CheckUpdate")
-compileCommon.CompileDbg("AltAppSwitcher")
-compileCommon.CompileRel("CheckUpdate")
-compileCommon.CompileRel("AltAppSwitcher")
