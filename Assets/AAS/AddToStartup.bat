526d86bb48602558ee874cb4aff2099ded9b4125
hdlx <hamtarodeluxe@gmail.com>
2025-08-13 22:19:11 +0200

    Do not ask for admin rights in .bat

--- Assets/AAS/AddToStartup.bat
+++ Assets/AAS/AddToStartup.bat
@@ -1,15 +1,6 @@
 @echo off
-REM https://superuser.com/questions/788924/is-it-possible-to-automatically-run-a-batch-file-as-administrator
 
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-if %errorlevel% neq 0 (
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" ", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-)
+setlocal ENABLEDELAYEDEXPANSION
 
 cd /d "%~dp0"
 
@@ -22,11 +13,24 @@ if not exist "%fullPath%" (
 
 schtasks /create /sc ONEVENT /ec Application /tn AltAppSwitcher /tr "'%fullPath%'" /RL HIGHEST /F
 
+if !errorlevel! neq 0 (
+    schtasks /create /sc ONEVENT /ec Application /tn AltAppSwitcher /tr "'%fullPath%'" /RL LIMITED /F
+    if !errorlevel! neq 0 (
+        msg * "Task creation failed."
+        exit
+    )
+    set "limited=true"
+)
+
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "AltAppSwitcher" /t REG_SZ /d "schtasks /run /tn AltAppSwitcher" /f
 
-if %errorlevel% equ 0 (
-    msg * "AltAppSwitcher has been added to startup apps. Please re-run this utility if you move the application files."
+if !errorlevel! neq 0 (
+    msg * "Adding task to run failed. If a previous task was created as an admin, please re-run this utility as an admin."
     exit
 )
 
-pause
+if "%limited%" == "true" (
+    msg * "Startup task added with limited rights. Re-run this utility as admin for admin rights.
+) else (
+    msg * "Startup task added. Re-run this utility when moving AltAppSwitcher.exe."
+)
--- Assets/AAS/RemoveFromStartup.bat
+++ Assets/AAS/RemoveFromStartup.bat
@@ -1,25 +1,18 @@
 @echo off
-REM https://superuser.com/questions/788924/is-it-possible-to-automatically-run-a-batch-file-as-administrator
 
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-if %errorlevel% neq 0 (
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" ", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-)
+setlocal ENABLEDELAYEDEXPANSION
 
 cd /d "%~dp0"
 
 schtasks /delete /tn AltAppSwitcher /f
-if %errorlevel% neq 0 (
-    msg * "No startup task to remove."
+if !errorlevel! neq 0 (
+    msg * "No startup task to remove. If the task was created as admin, please re-run this utility as admin."
     exit
 )
+
 reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "AltAppSwitcher" /f
-if %errorlevel% neq 0 (
+
+if !errorlevel! neq 0 (
     msg * "No startup task to remove."
     exit
 )
