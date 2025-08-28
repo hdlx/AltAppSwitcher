c6f1fa25193d48e7fbf2bf33fe77791154265ece
hdlx <hamtarodeluxe@gmail.com>
2025-08-28 22:58:33 +0200

    Removed admin rights prompt for close script (might be flagged malicious)

--- Assets/AAS/CloseAltAppSwitcher.bat
+++ Assets/AAS/CloseAltAppSwitcher.bat
@@ -1,15 +1,4 @@
 @echo off
-REM https://superuser.com/questions/788924/is-it-possible-to-automatically-run-a-batch-file-as-administrator
-
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-if %errorlevel% neq 0 (
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" ", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-)
 
 cd /d "%~dp0"
 
