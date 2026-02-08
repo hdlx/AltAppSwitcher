badfcbb17f724b942bc85954f124f4b4f8068d36
hdlx <hamtarodeluxe@gmail.com>
2026-02-08 14:04:30 +0100

    Fix archive input path

--- DeployAll.bat
+++ DeployAll.bat
@@ -1,5 +1,5 @@
 mingw32-make.exe clean
 mingw32-make.exe ARCH=x86_64 CONF=Release
 mingw32-make.exe ARCH=aarch64 CONF=Release
-python AAS.py MakeArchive Output/Release_x86_64
-python AAS.py MakeArchive Output/Release_aarch64
+python AAS.py MakeArchive Output/Release_x86_64/AAS
+python AAS.py MakeArchive Output/Release_aarch64/AAS
