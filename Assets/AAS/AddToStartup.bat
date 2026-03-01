798f594bee767a3a128bbc1e4aa814ee749963b1
alex <alex.porubaimikh@outlook.com>
2026-03-01 22:24:52 +0100

    make task scheduler start app on logon and unlock

--- Assets/AAS/AddToStartup.bat
+++ Assets/AAS/AddToStartup.bat
@@ -11,10 +11,9 @@ if not exist "%fullPath%" (
     exit
 )
 
-schtasks /create /sc ONEVENT /ec Application /tn AltAppSwitcher /tr "'%fullPath%'" /RL HIGHEST /F
-
+call :createTask HighestAvailable
 if !errorlevel! neq 0 (
-    schtasks /create /sc ONEVENT /ec Application /tn AltAppSwitcher /tr "'%fullPath%'" /RL LIMITED /F
+    call :createTask LeastPrivilege
     if !errorlevel! neq 0 (
         msg * "Task creation failed. If a previous task was created with admin privileges, please re-run this utility as an admin."
         exit
@@ -30,7 +29,52 @@ if !errorlevel! neq 0 (
 )
 
 if "%limited%" == "true" (
-    msg * "Startup task added with limited rights. Re-run this utility as admin for admin rights.
+    msg * "Startup task added with limited rights. Re-run this utility as admin for admin rights."
 ) else (
     msg * "Startup task added. Re-run this utility when moving AltAppSwitcher.exe."
 )
+exit /b
+
+:createTask
+set "curUser=%USERDOMAIN%\%USERNAME%"
+set "xmlFile=%TEMP%\AltAppSwitcher_task.xml"
+(
+echo ^<?xml version="1.0" encoding="UTF-16"?^>
+echo ^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
+echo   ^<Triggers^>
+echo     ^<LogonTrigger^>
+echo       ^<Enabled^>true^</Enabled^>
+echo       ^<UserId^>%curUser%^</UserId^>
+echo     ^</LogonTrigger^>
+echo     ^<SessionStateChangeTrigger^>
+echo       ^<Enabled^>true^</Enabled^>
+echo       ^<StateChange^>SessionUnlock^</StateChange^>
+echo       ^<UserId^>%curUser%^</UserId^>
+echo     ^</SessionStateChangeTrigger^>
+echo   ^</Triggers^>
+echo   ^<Principals^>
+echo     ^<Principal id="Author"^>
+echo       ^<LogonType^>InteractiveToken^</LogonType^>
+echo       ^<RunLevel^>%1^</RunLevel^>
+echo     ^</Principal^>
+echo   ^</Principals^>
+echo   ^<Settings^>
+echo     ^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>
+echo     ^<DisallowStartIfOnBatteries^>false^</DisallowStartIfOnBatteries^>
+echo     ^<StopIfGoingOnBatteries^>false^</StopIfGoingOnBatteries^>
+echo     ^<ExecutionTimeLimit^>PT0S^</ExecutionTimeLimit^>
+echo     ^<Priority^>7^</Priority^>
+echo   ^</Settings^>
+echo   ^<Actions^>
+echo     ^<Exec^>
+echo       ^<Command^>%fullPath%^</Command^>
+echo     ^</Exec^>
+echo   ^</Actions^>
+echo ^</Task^>
+) > "%xmlFile%"
+
+schtasks /create /tn AltAppSwitcher /xml "%xmlFile%" /F
+
+set "result=!errorlevel!"
+del "%xmlFile%"
+exit /b !result!
